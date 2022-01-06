ccbd761fae5a7f5c43b9b0b18c6433f2c7500b65
Martin Soengas <martinsoengas@Martins-Macbook-Pro.local>
2022-01-06 19:07:46 -0300

    NewExpense UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -30,12 +31,10 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <div>
-        <Expenses items={expenses} />
-      </div>
+      <NewExpense />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title); // useState is a react hook! (they all start with "use" and be called inside functions)
+
+  const clickHandler = () => {
+    setTitle("Updated");
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
